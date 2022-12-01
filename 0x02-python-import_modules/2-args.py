419ef620000d72f0d0d003f05eea9930ee38a1e5
Fatumayattani <fyattani@gmail.com>
2022-12-01 09:58:42 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,22 +4,24 @@ if __name__ == "__main__":
 
     import sys
 
+    args = len(sys.argv)
 
+    print("{} ".format(args - 1), end="")
 
-if len(sys.argv) == 1:
+    if args == 1:
 
-    print("{} arguments.".format(len(sys.argv) - 1))
+        print("arguments.")
 
-elif len(sys.argv) == 2:
+    elif args > 1:
 
-    print("{} argument:".format(len(sys.argv) - 1))
+        if args == 2:
 
-else:
+            print("argument:")
 
-    print("{} arguments:".format(len(sys.argv) - 1))
+        elif args > 2:
 
+            print("arguments:")
 
+        for i in range(args - 1):
 
-for a in range(1, len(sys.argv)):
-
-    print("{}: {}".format(a, sys.argv[a]))
+            print("{}: {}".format((i + 1), sys.argv[i + 1]))
