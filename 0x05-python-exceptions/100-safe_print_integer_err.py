f8f1b09ca93f46d214ff1cf33405c50e06637fb4
Fatumayattani <fyattani@gmail.com>
2022-12-19 10:54:24 +0000

    integer err

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+
+    try:
+
+        print("{:d}".format(value))
+
+        return True
+
+    except Exception as e:
+
+        print("Exception:", e, file=sys.stderr)
+
+        return False
