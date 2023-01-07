dfee925bff7cefccc0db691f0401ea84722e4cf6
Fatumayattani <fyattani@gmail.com>
2023-01-07 13:05:21 +0000

    save changes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,34 @@
-#!/usr/bin/python3
+!/usr/bin/python3
+
 """
-add_integer:
-    Checks if parameters are int
-    Returns sum of parameters
+This is is the "0-add_integer" module.
+
+The 0-add_integer module supplies one function, add_integer(a, b).
+
 """
 
 
-def add_integer(a, b=98):
-    """
-    Checks if int, otherwise return sum
-    """
-    if type(a) == float or type(b) == float:
+
+
+
+def add_integer(a, b):
+
+    """Return the addition of two numbers."""
+
+    if type(a) is not int and type(a) is not float:
+
+        raise TypeError("a must be an integer")
+
+    if type(b) is not int and type(b) is not float:
+
+        raise TypeError("b must be an integer")
+
+    if type(a) is float:
+
         a = int(a)
+
+    if type(b) is float:
+
         b = int(b)
 
-    if type(a) != int:
-        raise TypeError("a must be an integer")
-    elif type(b) != int:
-        raise TypeError("b must be an integer")
-    else:
-        return a + b
+    return a + b
--- 0x07-python-test_driven_development/tests/5-main.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-
-text_indentation = __import__('5-text_indentation').text_indentation
-text_indentation("""Importing a module in Python is done using the import statement. The __import__() function is a lower-level function that is used to import a module. It takes the name of the module to import as a string and returns the module object""")
