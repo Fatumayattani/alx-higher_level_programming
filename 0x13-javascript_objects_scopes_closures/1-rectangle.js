e3e2be4a4ee570c3b3cdcd278adbf42cb071036c
Fatumayattani <fyattani@gmail.com>
2023-03-14 06:28:03 -0700

    update files

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Javascript that define and initializes a class.
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
